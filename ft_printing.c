232f4152c9746f4109b573fbe149d55bf413394e
Arthur <arthurtweak@gmail.com>
2020-12-09 23:06:40 +0100

    auto commit

--- ft_printing.c
+++ ft_printing.c
@@ -23,8 +23,10 @@ void ft_print_flag(t_options print, va_list *args, int *count)
         *count += ft_print_number(print, va_arg(*args, unsigned int));
     else if(print.type == 's')
         *count += ft_print_str(print, va_arg(*args, char *));
-    else if(print.type == 'c' || print.type == '%')
+    else if(print.type == 'c')
         *count += ft_print_char(print, va_arg(*args, int));
+    else if(print.type == '%')
+        *count += ft_print_char(print, '%');
     else if(print.type == 'p')
         write(1, "en cours", 8);
     //printf("type: %c, reverse_padd: %i, get_sign: %d, padd_char: %c, padd_size: %d, field_size: %d\n", print.type, print.reverse_padd, print.get_sign, print.padd_char, print.padd_size, print.field_size);
@@ -37,10 +39,7 @@ int ft_print_char(t_options print, char c)
     count = 0;
     if(!print.reverse_padd)
         count += ft_print_padd(print.type, print.padd_size, 1, print.padd_char);
-    if(print.type == '%')
-        ft_putchar('%');
-    else
-        ft_putchar(c);
+    ft_putchar(c);
     count++;
     if(print.reverse_padd)
         count += ft_print_padd(print.type, print.padd_size, 1, ' ');
