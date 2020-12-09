d40feb588d260abab3d8cb25a43e882b6522703d
Arthur <arthurtweak@gmail.com>
2020-12-09 23:00:48 +0100

    auto commit

--- ft_parsing.c
+++ ft_parsing.c
@@ -26,6 +26,18 @@ int ft_parsing(va_list *args, char *s)
     return (count);
 }
 
+int to_plus(t_options *p, int nbr)
+{
+    if(nbr < 0)
+    {
+        p->reverse_padd = true;
+        p->padd_char = ' ';
+        return (nbr * -1);
+    }
+    else
+        return (nbr);
+}
+
 void ft_get_flag(char *str, t_options *print, int *main_loop, va_list *args)
 {
     int i;
@@ -39,12 +51,12 @@ void ft_get_flag(char *str, t_options *print, int *main_loop, va_list *args)
             print->reverse_padd = true;
         if(str[i] == '+')
             print->get_sign = true;
-        if(str[i] == '0' && (ft_isdigit(str[i + 1]) && str[i + 1] != '0'))
+        if(str[i] == '0')
             print->padd_char = '0';
         if(ft_isdigit(str[i]) && str[i] != '0')
             digit_getter(str + i, &print->padd_size, &i, args); 
         if(str[i] == '*')
-            print->padd_size = va_arg(*args, int);  
+            print->padd_size = to_plus(print, va_arg(*args, int));  
         if(str[i] == '.') {
             digit_getter(str + i + 1, &print->field_size, &i, args);
             print->padd_char = ' ';
