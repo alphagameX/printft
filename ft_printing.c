e08a34289a99f3fb83198c6953bfc3ab98648f2b
Arthur <arthurtweak@gmail.com>
2020-12-09 20:22:41 +0100

    auto commit

--- ft_printing.c
+++ ft_printing.c
@@ -13,14 +13,12 @@ void ft_print_flag(t_options print, va_list *args, int *count)
 {
     if(print.type == 'd' || print.type == 'i')
        *count += ft_print_number(print, va_arg(*args, int));
-    else if(print.type == 'u')
+    else if(print.type == 'u' || print.type == 'x' || print.type == 'X')
         *count += ft_print_number(print, va_arg(*args, unsigned int));
     else if(print.type == 's')
         *count += ft_print_str(print, va_arg(*args, char *));
     else if(print.type == 'c')
         ft_print_char(print, va_arg(*args, int));
-    else if(print.type == 'x' || print.type == 'X')
-        ft_print_hex(print, va_arg(*args, unsigned long));
     else if(print.type == 'p')
         write(1, "en cours", 8);
     //printf("type: %c, reverse_padd: %i, get_sign: %d, padd_char: %c, padd_size: %d, field_size: %d\n", print.type, print.reverse_padd, print.get_sign, print.padd_char, print.padd_size, print.field_size);
