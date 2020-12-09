879d8a6d2ae12cdcba66133a234303564b58a2d9
Arthur <arthurtweak@gmail.com>
2020-12-09 22:32:50 +0100

    auto commit

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main(void) {
 
 
-    int a = ft_printf("%0*i\n",7, -54);
-    int b = printf("%0*i\n",7, -54);
+    int a = ft_printf("%0*i\n",-7, -54);
+    int b = printf("%0*i\n",-7, -54);
 
     printf("%d\n%d\n", a, b);
 
